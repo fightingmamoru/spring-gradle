d19c6a73c7f0e4fdbb76ea19d9d878a31a1507aa
fightingmamoru@gmail.com <ma131026mo*>
2016-06-02 19:27:16 +0900

    Add log4j2, slf4j Dependency. And add Logger.

--- springweb/src/main/java/com/mamoru/webapp/hellospring/controller/HelloSpringController.java
+++ springweb/src/main/java/com/mamoru/webapp/hellospring/controller/HelloSpringController.java
@@ -1,5 +1,7 @@
 package com.mamoru.webapp.hellospring.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -7,17 +9,28 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class HelloSpringController
 {
+	// Add Logger
+	private static final Logger LOGGER = LoggerFactory.getLogger(HelloSpringController.class);
+
 	private static final String JSP_PATH = "hellospring/";
 
 	@RequestMapping(value = "/helloSpring")
 	public ModelAndView hello()
 	{
+		// Add log for controller start
+		String logMessage = "START HelloSpringController";
+		LOGGER.debug("HelloSpringLogger - {}", logMessage);
+
 		ModelAndView mav = new ModelAndView();
 
 		mav.setViewName(JSP_PATH + "hellospring");
 
 		mav.addObject("title", "[Spring Web MVC with Gradle/Java Config]");
 		mav.addObject("message", "Hello Spring! - with Gradle and Java Config");
+
+		// Add log for controller end
+		logMessage = "END HelloSpringController";
+		LOGGER.debug("HelloSpringLogger - {}", logMessage);
 
 		return mav;
 	}
